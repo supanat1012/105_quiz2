736c3fd78626d65fa759f8a7dff4df1343aa8249
supanat <supanat.ap@gmail.com>
2021-03-25 17:38:26 +0700

    a

--- 105_quiz2_1.c
+++ 105_quiz2_1.c
@@ -2,23 +2,35 @@
 int main()
 {
     int n,i,j,a;
-    int A[100],B[100],result[100];
+    int A[100],B[100],cubefree[100];
 
     scanf("%d",&n);
 
-    for(i=2;i<=100;i++)
-    {
-        A[i-1]=i*i*i;
-    }
     for(i=1;i<=100;i++)
     {
-        if((A[i]%i)==0)
-            result[i]=i;
+        A[i]=i*i*i;
+        //printf("%d,",A[i]);
     }
 
-    for(i=1;i<=100;i++)
+    for(i=1,j=1;i<=100;i++)
     {
-       printf("%d,",result[i]);
+        if(A[i]%i==0)
+          {
+              cubefree[j]=i;
+              printf("%d,",cubefree[j]);
+              j++;
+
+          }
     }
+
+
+   /* for(i=1;i<=n;i++)
+    {
+        if(n==result[i])
+        {
+            printf("%d",i);
+        }
+
+    }*/
 
 }
