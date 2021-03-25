a14f11b7616e263e23fcc82ffe53cf38bfe961c6
supanat <supanat.ap@gmail.com>
2021-03-25 17:19:18 +0700

    a

--- 105_quiz2_1.c
+++ 105_quiz2_1.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a;
+    int A[100],B[100],result[100];
 
+    scanf("%d",&n);
+
+    for(i=2;i<=100;i++)
+    {
+        A[i-1]=i*i*i;
+    }
+    for(i=1;i<=100;i++)
+    {
+        if((A[i]%i)==0)
+            result[i]=i;
+    }
+
+    for(i=1;i<=100;i++)
+    {
+       printf("%d,",result[i]);
+    }
+
+}
